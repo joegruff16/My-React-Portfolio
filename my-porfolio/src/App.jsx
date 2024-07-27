d890849d454a400c9ee5b67e3578969a21cc5662
Joseph Ruff <joegruff16@gmail.com>
2024-07-27 15:09:31 -0400

    Finally got my app to render correctly. Playing with styles and the bootstrap library to determine how I'm going to style my Portfolio.

--- my-porfolio/src/App.jsx
+++ my-porfolio/src/App.jsx
@@ -1,19 +1,38 @@
-import React, { useState } from "react";
+import React from "react";
 import Footer from "./components/Footer";
-// import "./App.css";
+import { Alert, Button, Card, Form } from "react-bootstrap";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="container">
-      <h1>My Portfolio</h1>
-      <div className="card">
-        <Nav />
+    <div className="App">
+      <header className="App-header">
+        <Form>
+          <Form.Group>
+            <Form.Label>Email Address</Form.Label>
+            <Form.Control type="email" placeholder="test@gmail.com" />
+            <Form.Text className="text-muted">
+              We'll never use your email
+            </Form.Text>
+          </Form.Group>
+        </Form>
+        <Card className="mb-3" style={{ color: "#0081A7" }}>
+          <Card.Img />
+          <Card.Body />
+          <Card.Title>Hello, my name is Joe.</Card.Title>
+          <Card.Text>Welcome to my Portfolio</Card.Text>
+        </Card>
+        <Button>Test Button</Button>
+        <Alert>Use this instead</Alert>
+
+        {/* <Nav />
         <Intro />
         <Porfolio />
         <Timeline />
-        <Contact />
+        <Contact /> */}
         <Footer />
-      </div>
+      </header>
     </div>
   );
 }
--- my-porfolio/src/main.jsx
+++ my-porfolio/src/main.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-// import "./index.css";
+import "./index.css";
 
 // Import Bootstrap to use for styling
-import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
