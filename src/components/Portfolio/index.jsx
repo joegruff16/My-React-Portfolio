6fc65617832f0806e349fda6114428df0b69b275
Joseph Ruff <joegruff16@gmail.com>
2024-08-07 22:52:56 -0400

    Added a container that contains rows and colomns to make 3 cards per row. Made this with a sample card holder to get ready to add my 6 projects in.

--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -1,39 +1,77 @@
 import React from "react";
-import { Card, Button } from "react-bootstrap/";
+import { Card, Button, Container, Row, Col } from "react-bootstrap/";
 
 // import stylesheets
-import "bootstrap/dist/css/bootstrap.min.css";
 import "../Portfolio/portfolioStyle.css";
 // Add all of your projects into the component to pass into the page
-function Portfolio() {
-  return (
-    <>
-      <Card style={{ width: "18rem" }}>
-        <Card.Img variant="top" className="imgStyle customImageClass" />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          {/* <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text> */}
-          <Button variant="secondary">Repo</Button>
-          <Button variant="secondary">Live site</Button>
-        </Card.Body>
-      </Card>
-      <Card style={{ width: "18rem" }}>
-        <Card.Img variant="right" className="imgStyle customImageClass" />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          {/* <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text> */}
-          <Button variant="secondary">Repo</Button>
-          <Button variant="secondary">Live site</Button>
-        </Card.Body>
-      </Card>
-    </>
-  );
-}
+
+const Portfolio = () => (
+  <Container>
+    <Row>
+      <Col md={4}>
+        <Card className="cardStyle" style={{ width: "18rem" }}>
+          <Card.Img variant="top" className="imgStyle customImageClass" />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Button variant="secondary">Repo</Button>
+            <Button variant="secondary">Live site</Button>
+          </Card.Body>
+        </Card>
+      </Col>
+      <Col md={4}>
+        <Card className="cardStyle" style={{ width: "18rem" }}>
+          <Card.Img variant="top" className="imgStyle customImageClass" />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Button variant="secondary">Repo</Button>
+            <Button variant="secondary">Live site</Button>
+          </Card.Body>
+        </Card>
+      </Col>
+      <Col md={4}>
+        <Card className="cardStyle" style={{ width: "18rem" }}>
+          <Card.Img variant="top" className="imgStyle customImageClass" />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Button variant="secondary">Repo</Button>
+            <Button variant="secondary">Live site</Button>
+          </Card.Body>
+        </Card>
+      </Col>
+    </Row>
+    <Row>
+      <Col md={4}>
+        <Card className="cardStyle" style={{ width: "18rem" }}>
+          <Card.Img variant="top" className="imgStyle customImageClass" />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Button variant="secondary">Repo</Button>
+            <Button variant="secondary">Live site</Button>
+          </Card.Body>
+        </Card>
+      </Col>
+      <Col md={4}>
+        <Card className="cardStyle" style={{ width: "18rem" }}>
+          <Card.Img variant="top" className="imgStyle customImageClass" />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Button variant="secondary">Repo</Button>
+            <Button variant="secondary">Live site</Button>
+          </Card.Body>
+        </Card>
+      </Col>
+      <Col md={4}>
+        <Card className="cardStyle" style={{ width: "18rem" }}>
+          <Card.Img variant="top" className="imgStyle customImageClass" />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Button variant="secondary">Repo</Button>
+            <Button variant="secondary">Live site</Button>
+          </Card.Body>
+        </Card>
+      </Col>
+    </Row>
+  </Container>
+);
 
 export default Portfolio;
