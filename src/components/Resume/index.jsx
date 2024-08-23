19857530ddc7ccd3aee9f95963d5a8c174bc3356
Joseph Ruff <joegruff16@gmail.com>
2024-08-22 20:31:19 -0400

    Updated my Resume with my most up to date resume.

--- src/components/Resume/index.jsx
+++ src/components/Resume/index.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { EmbedPDF } from "@simplepdf/react-embed-pdf";
+import { DiBlackberry } from "react-icons/di";
+import { BiColor } from "react-icons/bi";
 
 // The PDF is displayed when rendering the component
 
@@ -9,8 +11,8 @@ function Resume() {
       <br />
       <EmbedPDF
         mode="inline"
-        style={{ width: 900, height: 800 }}
-        documentURL="https://h1nqc3w0.simplepdf.eu/form/78f5649037515be03e1590c5afff947cf342ef031ac9a3caeed7fa68f54333ef"
+        style={{ width: 800, height: 800 }}
+        documentURL="https://h1nqc3w0.simplepdf.eu/form/f91ce012884f9ecf27414942cdcc809fbeb013573b7c890110160cfa61dc2827"
       />
     </>
   );
